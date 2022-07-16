d1c5167002229a82d2904f2e9766b85d4860dfed
Ermias <35461600+ermiaskidane@users.noreply.github.com>
2022-07-16 19:12:43 +0100

    Update ClientRow.jsx

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -6,7 +6,7 @@ import { GET_CLIENTS } from '../queries/clientQueries'
 export default function ClientRow({ client }) {
   const [deleteClient] = useMutation(DELETE_CLIENT, {
     variables: { id: client.id },
-    // refetchQueries: [{ query: GET_CLIENTS }],
+    refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }],
     update(cache, { data: { deleteClient } }) {
       const { clients } = cache.readQuery({ query: GET_CLIENTS })
       cache.writeQuery({
